32507fbd373d00f4e9d71ff1d4df395e1850a5f7
bardia-keshavarzi <bardia.keshavarzi@gmail.com>
2025-10-22 16:15:40 +0330

    fix:reverts back to L1 loss only

--- src/training/losses.py
+++ src/training/losses.py
@@ -1,26 +1,6 @@
+# src/training/losses.py
 import torch.nn as nn
-import torch
-from torchvision.models import vgg16
-
-class PerceptualLoss(nn.Module):
-    def __init__(self):
-        super().__init__()
-        vgg = vgg16(pretrained=True).features[:16]
-        for param in vgg.parameters():
-            param.requires_grad = False
-        self.vgg = vgg.eval()
-
-    def forward(self, pred, target):
-        device = pred.device
-        self.vgg = self.vgg.to(device)
-        # inputs must be 0-1, 3 channels
-        return nn.functional.l1_loss(
-            self.vgg((pred+1)/2), self.vgg((target+1)/2)
-    )
 
 def get_loss_function():
-    l1 = nn.L1Loss()
-    perceptual = PerceptualLoss()
-    def loss_fn(pred, target):
-        return l1(pred, target) + 0.1 * perceptual(pred, target)
-    return loss_fn
+    """Return L1 loss only"""
+    return nn.L1Loss()
